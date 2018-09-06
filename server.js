705afd871a0b2164e5fe4b4716351ffd5d262c25
Throwback74 <corey.slade@gmail.com>
2018-09-05 17:14:47 -0700

    initial commit to new repo

---
+++ server.js
@@ -0,0 +1,33 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+var app = express();
+var PORT;
+
+var portNum = parseInt(process.argv[2]);
+
+if(Number.isInteger(portNum)) {
+  PORT = process.env.PORT || portNum;
+}else if(Number.isInteger(portNum) === false){
+  PORT = process.env.PORT || 3000;
+}
+
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
+// Router
+// require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
+require(path.join(__dirname, "./routing/htmlRoutes"))(app);
+
+// Listener
+app.listen(PORT, function() {
+  console.log("Portfolio App is listening on PORT: " + PORT);
+  console.log("App active at http://localhost:" + PORT);
+});
